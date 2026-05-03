29d9dbb694acda8352cedf34681b077688c75783
UglyGameFace <85308991+UglyGameFace@users.noreply.github.com>
2026-05-02 20:03:06 -0400

    Add guild config resolver package

---
+++ stoney_verify/config_new/__init__.py
@@ -0,0 +1,17 @@
+from .guild_config import (
+    GuildConfig,
+    clear_guild_config_cache,
+    get_guild_config,
+    resolve_configured_text_channel,
+    resolve_configured_role,
+    resolve_configured_category,
+)
+
+__all__ = [
+    "GuildConfig",
+    "clear_guild_config_cache",
+    "get_guild_config",
+    "resolve_configured_text_channel",
+    "resolve_configured_role",
+    "resolve_configured_category",
+]
