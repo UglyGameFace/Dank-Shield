1b1ffd85804ae6f60679079fa9fa4a7156345531
UglyGameFace <85308991+UglyGameFace@users.noreply.github.com>
2026-05-08 10:32:38 -0400

    Add setup template package

---
+++ stoney_verify/setup_new/__init__.py
@@ -0,0 +1,19 @@
+"""Plain-language setup template helpers for Dank Shield."""
+
+from .templates import (
+    SETUP_TEMPLATE_CHOICES,
+    SetupTemplateChoice,
+    build_setup_template_embed,
+    build_setup_template_select_options,
+    get_setup_template,
+    setup_template_payload,
+)
+
+__all__ = [
+    "SETUP_TEMPLATE_CHOICES",
+    "SetupTemplateChoice",
+    "build_setup_template_embed",
+    "build_setup_template_select_options",
+    "get_setup_template",
+    "setup_template_payload",
+]
