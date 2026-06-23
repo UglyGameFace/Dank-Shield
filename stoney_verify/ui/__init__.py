095172b4161a4f0ec787cd132f99cc54202f77f0
UglyGameFace <85308991+UglyGameFace@users.noreply.github.com>
2026-06-22 23:40:51 -0400

    Add shared Dank picker package

---
+++ stoney_verify/ui/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from .picker import (
+    DankChoice,
+    DankPickerView,
+    PickerAction,
+    chunk_choices,
+    make_choice,
+    make_home_choice,
+)
+
+__all__ = [
+    "DankChoice",
+    "DankPickerView",
+    "PickerAction",
+    "chunk_choices",
+    "make_choice",
+    "make_home_choice",
+]
