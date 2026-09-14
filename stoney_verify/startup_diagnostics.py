0465a140e55651d6417311c2dbe33e3ad602c8ac
UglyGameFace <85308991+UglyGameFace@users.noreply.github.com>
2026-09-14 17:49:46 -0400

    Track native command runtime in startup diagnostics

--- stoney_verify/startup_diagnostics.py
+++ stoney_verify/startup_diagnostics.py
@@ -13,6 +13,7 @@ from typing import Any, Dict, List, Optional, Sequence
 EXPECTED_STARTUP_OWNER_MODULES: tuple[str, ...] = (
     "stoney_verify.startup_guards.process_health",
     "stoney_verify.startup_guards.discord_api_safety",
+    "stoney_verify.command_runtime",
     "stoney_verify.startup_guards.public_server_env_id_guard",
     "stoney_verify.startup_guards.guild_config_runtime_validator",
     "stoney_verify.startup_guards.interaction_action_lock_guard",
