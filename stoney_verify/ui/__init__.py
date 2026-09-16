d03ea3b8ba8f1e293f42ed2fe121bf79f7396332
UglyGameFace <85308991+UglyGameFace@users.noreply.github.com>
2026-09-16 01:10:27 -0400

    Export shared guild resource browser

--- stoney_verify/ui/__init__.py
+++ stoney_verify/ui/__init__.py
@@ -16,19 +16,33 @@ from .picker import (
     make_choice,
     make_home_choice,
 )
+from .resource_browser import (
+    DankGuildResourceBrowserView,
+    DankResourceCandidate,
+    DankResourceSearchModal,
+    ResourcePickAction,
+    ResourcePredicate,
+    build_resource_candidates,
+)
 
 __all__ = [
     "ChannelPickAction",
     "DankChannelSelect",
     "DankChoice",
+    "DankGuildResourceBrowserView",
     "DankMentionableSelect",
     "DankPickerView",
+    "DankResourceCandidate",
+    "DankResourceSearchModal",
     "DankRoleSelect",
     "DankUserSelect",
     "MentionablePickAction",
     "PickerAction",
+    "ResourcePickAction",
+    "ResourcePredicate",
     "RolePickAction",
     "UserPickAction",
+    "build_resource_candidates",
     "chunk_choices",
     "make_choice",
     "make_home_choice",
